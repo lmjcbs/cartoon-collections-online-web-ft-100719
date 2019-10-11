e3291ddd83243ad57af329f35ba092ee1c44b204
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-11 14:20:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese = nil
-  array.each_with_index { |ingredient, i| ingredient.include?(cheese_types)}
+  array.each_with_index do|ingredient, i| ingredient.include?(cheese_types)}
 end
