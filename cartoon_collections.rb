87b1c4f822630566e905fe8bd970075c09e0bc0c
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-11 14:18:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 
 def long_planeteer_calls(planeteer_calls)# code an argument here
   # Your code here
-  planeteer_calls.any? { |call| call.size > 4}
+  planeteer_calls.any? { |call| call.size > 4 }
 end
 
 def find_the_cheese# code an argument here
