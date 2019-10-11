c5aefc375123650d7d79b536191efc9db63aaaa3
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-11 14:21:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,4 +19,5 @@ def find_the_cheese(array)# code an argument here
   cheese = nil
   array.each_with_index do |ingredient, i|
     if ingredient.include?(cheese_types)
+      cheese = array[i]
 end
